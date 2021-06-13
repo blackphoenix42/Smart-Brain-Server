8516ee34812ce997245fb37b3550f53b23b9627c
Ayush Yadav <aayush.sang@gmail.com>
2021-06-14 00:26:29 +0530

    Add transactions

--- server.js
+++ server.js
@@ -56,17 +56,29 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body
-
-    db('users')
-        .returning('*')
-        .insert({
-            email,
-            name,
-            joined: new Date()
+    const hash = bcrypt.hashSync(password)
+    db.transaction(trx => {
+        trx.insert({
+            hash,
+            email
         })
-        .then(user => {
-            res.json(user[0])
-        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0])
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
         .catch(err => res.staus(400).json('unable to register'))
 
 
